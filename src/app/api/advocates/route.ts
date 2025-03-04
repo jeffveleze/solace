81754c3ddbf01e1c2dd6818308c474477009e250
jeffveleze <jeff.v@verytechnology.com>
2025-03-03 20:32:11 -0500

    Added prettier, added env example file, removed .env file, formatted main page and enable db data

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,13 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import db from '../../../db';
+import { advocates } from '../../../db/schema';
+// import { advocateData } from "../../../db/seed/advocates"; // Uncomment this line to use the hardcoded seed data
 
 export async function GET() {
   // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+  const data = await db.select().from(advocates);
 
-  const data = advocateData;
+  // Uncomment this line to use the hardcoded seed data
+  // const data = advocateData;
 
   return Response.json({ data });
 }
